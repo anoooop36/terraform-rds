e1a14ff38e15a2bd6cb5cf35c506f283e13468a6
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-08-03 10:15:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,9 +17,20 @@ resource "aws_db_instance" "default" {
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
-  name                 = "mydbRds123"
+  db_name                 = "mydbRds123"
   username             = "foo"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
+  db_subnet_group_name = "${aws_db_subnet_group.db_subnet_group.id}"
+}
+
+
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = ["vpc-0ee8ceb17cfd9e53b"]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
 }
