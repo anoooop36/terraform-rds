97010302c6edd4ce8f153471a818cbb3ea2ab997
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2025-06-21 13:54:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.85.0"
+    }
+  }
+}
+
 provider "aws" {
  access_key = var.access_key
  secret_key = var.secret_key
@@ -22,27 +31,5 @@ resource "aws_db_instance" "default" {
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
-  db_subnet_group_name = "${aws_db_subnet_group.dbSubnet.name}"
-}
-
-
-resource "aws_db_subnet_group" "dbSubnet" {
-  name       = "rds"
-  subnet_ids  = [aws_subnet.rds_subnet.id, aws_subnet.rds_subnet1.id]
-
-  tags = {
-    Name = "My DB subnet group"
-  }
-}
-
-resource "aws_subnet" "rds_subnet" {
-  vpc_id     = "vpc-0ee8ceb17cfd9e53b"
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-}
-
-resource "aws_subnet" "rds_subnet1" {
-  vpc_id     = "vpc-0ee8ceb17cfd9e53b"
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+  db_subnet_group_name = "main"
 }
